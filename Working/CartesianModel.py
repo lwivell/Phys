Fix tanh overflow problem

Working/CartesianModel.py
@@ -19,7 +19,7 @@ thick=1
 
 magperm = mu*meab
 
-thick1 = 4.5
+thick1 = 450000
 cond1 = 10**6
 dieperm1 = 10*miti
 
@@ -30,5 +30,3 @@ apparimp1 = appar_imp(wave1, intrin1, intrin1, thick1)
 print(wave1)
 print(apparimp1)
 print(intrin1)
-
-print(np.tanh(5j))

Working/Extractables/ElectromagProps.py
@@ -68,8 +68,14 @@ def appar_imp(wavenumber, intrinimp, prevapparimp, thick):
     apparimp
         Apparent impedance, Z, of the layer being modelled
     """    
+    tanhval = ((1j)*wavenumber*thick)          
+    if np.linalg.norm(tanhval) >= 100:         #This for loop simply clips the tanh value as it can get too large for python to handle, causes overflow error
+        if np.real(tanhval) >= 5:
+            tanhval = 10 + 10j
+        else:
+            tanhval = -10 -10j
 
-    Z =np.complex128(intrinimp * (prevapparimp + (intrinimp*np.tanh((1j)*wavenumber*thick)))/(intrinimp + (prevapparimp*np.tanh((1j)*wavenumber*thick))))
+    Z =np.complex128(intrinimp * (prevapparimp + (intrinimp*np.tanh(tanhval)))/(intrinimp + (prevapparimp*np.tanh(tanhval))))
     return Z
 
 def appar_resis(apparimp, angfreq, magperm):