385a645745b48919c8d02eab079eb317a588ff5f
LWivell <lukewivell01@gmail.com>
2024-10-11 16:22:02 +0100

    Import of functions, gitignore installation

--- Working/CartesianModel.py
+++ Working/CartesianModel.py
@@ -1,23 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.constants as const
+from Extractables.ElectromagProps import wavenumber
+from Extractables.ElectromagProps import intrin_imp
+from Extractables.ElectromagProps import appar_imp
+from Extractables.ElectromagProps import appar_resis
+
 
 miti = const.eps0.value
 meab = const.mu0.value
 
-omega = 0
+angfreq = 0
 mu = 1
 epsilon = 1
-cond = 1
+elecon = 1
 prevappar = 1
-h=1
+thick=1
 
-k = (((omega**2)*mu*meab*epsilon*miti)-((1j)*omega*mu*meab*cond))**(1/2)
-lowk = (-(1j)*omega*mu*meab*cond)**(1/2)
-highk = ((omega**2)*mu*meab*epsilon*miti)
-
-
-intrinimp = (((1j)*mu*meab*omega)/(cond+((1j)*omega*epsilon*miti)))**(1/2)
-
-apparimp = intrinimp*((prevappar+(intrinimp*np.tanh((1j)*k*h)))/(intrinimp+(prevappar*np.tanh((1j)*k*h))))
+dieperm = epsilon*miti
+magperm = mu*meab
 
