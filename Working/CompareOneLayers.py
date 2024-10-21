547662191a603fe6dbe6e2c71363ecc7a24ee16b
lukewivell <lukewivell01@gmail.com>
2024-10-21 16:38:42 +0100

    Layer build comparisons

--- Working/CompareOneLayers.py
+++ Working/CompareOneLayers.py
@@ -26,7 +26,7 @@ start = 1
 end = 2000000000000000000000000000000000
 stepsize = 1
 
-thickness = 1560*1000
+thickness = 10*1000
 conductivity = 10**(-4)
 dieperm = 3.5*miti
 
@@ -54,36 +54,53 @@ for entry in resistivity:
 conductivity = 10**(-3)
 dieperm = 5*miti
 
-resistivity =[]
 
 start = 1
 end = 2000000000000000000000000000000000
 stepsize = 1
+
+apparimps=[]
+resistivities=[]
 
 while start< end:
         
         angfreq=start/(10**13)
         angfreqs.append(angfreq)
 
-        intrinsic = intrin_imp(angfreq, meab, conductivity, dieperm)
-        intrin.append(np.linalg.norm(intrinsic))
-        squareintrin = np.linalg.norm(intrinsic)**2
-        squintrin.append(squareintrin)
-        resis = appar_resis(intrinsic, angfreq, meab)
-        resistivity.append(resis)
+        magperm1 = 5000*meab                  #Water
+        thick1 = 100*1000
+        cond1 = 1
+        dieperm1 = 85*miti
+
+        wave1 = wavenumber(angfreq, magperm1, cond1, dieperm1)
+        intrin1 = intrin_imp(angfreq, magperm1, cond1, dieperm1)
+        apparimp1 = appar_imp(wave1, intrin1, intrin1, thick1)
+        apparimps.append(apparimp1)
+
+        magperm2 = 1*meab                 #Ice
+        thick2 = 10*1000
+        cond2 = 10**(-4)
+        dieperm2 = 3.5*miti
+
+        wave2 = wavenumber(angfreq, magperm2, cond2, dieperm2)
+        intrin2 = intrin_imp(angfreq, magperm2, cond2, dieperm2)
+        apparimp2 = appar_imp(wave2, intrin2, apparimp1, thick2)
+        apparimps.append(apparimp2)
+
         start += stepsize
         stepsize *= 1.05
+        resistivity = appar_resis(apparimps[-1], angfreq, meab)
+        resistivities.append(resistivity)
 
 
-
-silicondu=[]
-for entry in resistivity:
-    silicondu.append(1/entry)
+twocondu=[]
+for entry in resistivities:
+    twocondu.append(1/entry)
 
 conductivity = 1
 dieperm = 85*miti
 
-resistivity =[]
+resistivithree =[]
 
 start = 1
 end = 2000000000000000000000000000000000
@@ -94,29 +111,58 @@ while start< end:
         angfreq=start/(10**13)
         angfreqs.append(angfreq)
 
-        intrinsic = intrin_imp(angfreq, meab, conductivity, dieperm)
-        intrin.append(np.linalg.norm(intrinsic))
-        squareintrin = np.linalg.norm(intrinsic)**2
-        squintrin.append(squareintrin)
-        resis = appar_resis(intrinsic, angfreq, meab)
-        resistivity.append(resis)
+        magperm1 = meab                  #Innermost layer     Silicate
+        thick1 = 1000*1000
+        cond1 = 10**(-3)
+        dieperm1 = 5*miti
+
+        wave1 = wavenumber(angfreq, magperm1, cond1, dieperm1)
+        intrin1 = intrin_imp(angfreq, magperm1, cond1, dieperm1)
+        apparimp1 = appar_imp(wave1, intrin1, intrin1, thick1)
+        apparimps.append(apparimp1)
+
+        magperm2 = meab                 #Second innermost layer    water
+        thick2 = 100*1000
+        cond2 = 1
+        dieperm2 = 85*miti
+
+        wave2 = wavenumber(angfreq, magperm2, cond2, dieperm2)
+        intrin2 = intrin_imp(angfreq, magperm2, cond2, dieperm2)
+        apparimp2 = appar_imp(wave2, intrin2, apparimp1, thick2)
+        apparimps.append(apparimp2)
+
+
+        magperm3 = meab                  #Third layer             ice
+        thick3 = 10*1000
+        cond3 = 10**(-4)
+        dieperm3 =3.5*miti
+
+        wave3 = wavenumber(angfreq, magperm3, cond3, dieperm3)
+        intrin3 = intrin_imp(angfreq, magperm3, cond3, dieperm3)
+        apparimp3 = appar_imp(wave3, intrin3, apparimp2, thick3)
+        apparimps.append(apparimp3)
+
+        resistivity = appar_resis(apparimps[-1], angfreq, meab)
+        resistivithree.append(resistivity)
+
+
         start += stepsize
         stepsize *= 1.05
 
 
 
-watercondu=[]
-for entry in resistivity:
-    watercondu.append(1/entry)
+threecondu=[]
+for entry in resistivithree:
+    threecondu.append(1/entry)
 
-plt.title("Single Layer Models")
-plt.plot(freqs, watercondu, label='Water')
-plt.plot(freqs, icecondu, label='Ice')
-plt.plot(freqs, silicondu, label='Silicate')
+plt.title("Addition of layers to model")
+plt.plot(freqs, twocondu, label='Ice + Water Layers')
+plt.plot(freqs, icecondu, label='Ice Layer')
+plt.plot(freqs, threecondu, label='Complete Model')
 plt.yscale('log')
 plt.xscale('log')
 plt.xlabel('Frequency [Hz]')
-plt.ylabel('Conductivity [S/m]')
+plt.ylabel('Apparent Conductivity [S/m]')
 plt.legend()
 plt.show()
 
